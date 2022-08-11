abd35aec59073ebbd73038e05dfeea3014cccf48
imabby0508 <imabby0508@gmail.com>
2022-08-11 18:18:39 +0800

    set handlebars

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
