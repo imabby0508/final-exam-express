cc533b57c1dd485ba8d691923e2f577a92287a51
imabby0508 <imabby0508@gmail.com>
2022-08-15 20:12:02 +0800

    adjust JavaScript Standard style

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,15 +11,3 @@ if (urlPath === "/portfolio") {
 if (urlPath === "/contact") {
   document.querySelector("#contact").className = "highlight";
 }
-
-// const buttonsWrapper = document.querySelector(".buttons-wrapper");
-// const buttons = document.querySelectorAll(".button");
-// console.log(buttonsWrapper);
-// buttonsWrapper.addEventListener("click", function buttonClicked(event) {
-//   console.log(event.target);
-//   console.log(buttons);
-//   for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].classList.add("highlight");
-//   }
-//   // event.target.classList.add("highlight");
-// });
